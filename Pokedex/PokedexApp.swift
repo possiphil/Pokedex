2339ba0109d0e09618e6bb1e8c6e839522655d35
Philipp Sanktjohanser <venture@mein.gmx>
2022-12-21 15:21:58 +0100

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Philipp Sanktjohanser on 21.12.22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
